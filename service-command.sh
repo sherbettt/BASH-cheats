d77a39b94ac3837af3be2609efd868ff2c07048e
Кирилл Кораблин <k@runtel.ru>
2026-04-24 16:21:40 +0300

    красоты для

--- service-command.sh
+++ service-command.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "=== Git Pull ==="
 git pull;
+echo "=== Git Push ==="
 git push git@gitflic.ru:kkorablin/bash-cheats.git;
 git push git@gitverse.ru:sherbettt/BASH-cheats.git;
+echo "=== The End ==="
