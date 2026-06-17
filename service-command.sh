8cbf2efb0348f4218c54f4c334c8218a0378aac9
Кирилл Кораблин <k@runtel.ru>
2026-06-17 11:17:35 +0300

    add new link (internal)

--- service-command.sh
+++ service-command.sh
@@ -5,4 +5,5 @@ git pull;
 echo "=== Git Push ==="
 git push git@gitflic.ru:kkorablin/bash-cheats.git;
 git push git@gitverse.ru:sherbettt/BASH-cheats.git;
+git push git@gitlab.runtel.org:kkorablin/bash-cheats.git;
 echo "=== The End ==="
