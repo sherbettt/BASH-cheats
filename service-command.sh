08a3a9d8678727aee30891ea8bd55207dec9779b
Kirill Korablin <kkorablin@yandex.ru>
2026-07-06 12:53:55 +0300

    printf

--- service-command.sh
+++ service-command.sh
@@ -7,14 +7,19 @@ YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 MAGENTA='\033[0;35m'
 CYAN='\033[0;36m'
-NC='\033[0m' # No Color (сброс)
+NC='\033[0m'
 
-echo -e "${GREEN}=== Git Pull ===${NC}"
+#echo -e "${GREEN}=== Git Pull ===${NC}"
+printf "${GREEN}=== Git Pull ===${NC}\n"
 git pull;
-echo "${YELLOW}=== Git Push to gitflic.ru ===${NC}"
+
+printf "${YELLOW}=== Git Push to gitflic.ru ===${NC}\n"
 git push git@gitflic.ru:kkorablin/bash-cheats.git;
-echo "${YELLOW}=== Git Push to gitverse.ru ===${NC}"
+
+printf "${YELLOW}=== Git Push to gitverse.ru ===${NC}\n"
 git push git@gitverse.ru:sherbettt/BASH-cheats.git;
-echo "${YELLOW}=== Git Push to gitlab.runtel.org ===${NC}"
+
+printf "${YELLOW}=== Git Push to gitlab.runtel.org ===${NC}\n"
 git push git@gitlab.runtel.org:kkorablin/bash-cheats.git;
-echo "${RED}=== The End ===${NC}"
+
+printf "${RED}=== The End ===${NC}\n"
