39add114a2d46e5b8ceab354c6562ed5ec23efc2
Kirill Korablin <kkorablin@yandex.ru>
2026-07-06 12:49:48 +0300

    update script

--- service-command.sh
+++ service-command.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-echo "=== Git Pull ==="
+# Определяем цвета
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color (сброс)
+
+echo -e "${GREEN}=== Git Pull ===${NC}"
 git pull;
-echo "=== Git Push ==="
+echo "${YELLOW}=== Git Push to gitflic.ru ===${NC}"
 git push git@gitflic.ru:kkorablin/bash-cheats.git;
+echo "${YELLOW}=== Git Push to gitverse.ru ===${NC}"
 git push git@gitverse.ru:sherbettt/BASH-cheats.git;
+echo "${YELLOW}=== Git Push to gitlab.runtel.org ===${NC}"
 git push git@gitlab.runtel.org:kkorablin/bash-cheats.git;
-echo "=== The End ==="
+echo "${CYAN}=== The End ===${NC}"
