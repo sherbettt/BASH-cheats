a8d2e42e1c7a5cc0fef7ab5dab34a6c1a7f617fd
Kirill K <44464893+sherbettt@users.noreply.github.com>
2026-07-13 13:16:42 +0300

    Create rustupc-update.sh

---
+++ scripts_and_utils/rustupc-update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Определяем цвета
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+
+printf "${YELLOW}=== rustup update stable ===${NC}\n"
+rustup update stable
+
+cargo --version
+rustc --version
+rustup --version
+
+printf "${YELLOW}=== rust utils list ===${NC}\n"
+cargo install --list
+
+printf "${YELLOW}=== cargo update utils ===${NC}\n"
+cargo install-update -a
