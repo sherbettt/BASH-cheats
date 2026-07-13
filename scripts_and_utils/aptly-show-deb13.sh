3745a24608c9fb712fce2dfa2285cc86f695a42b
Kirill K <44464893+sherbettt@users.noreply.github.com>
2026-07-13 13:24:39 +0300

    Create aptly-show-deb13.sh

---
+++ scripts_and_utils/aptly-show-deb13.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Определяем цвета
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color (сброс)
+
+
+printf "${YELLOW}=== aptly repo list ===${NC}\n"
+aptly repo list 
+echo
+
+printf "${YELLOW}=== aptly repo show runtel-trixie-dev ===${NC}\n"
+aptly repo show runtel-trixie-dev
+echo
+
+printf "${YELLOW}=== aptly repo show runtel-trixie ===${NC}\n"
+aptly repo show runtel-trixie
+echo
+
+printf "${YELLOW}=== aptly repo search runtel-trixie (25 objects) ===${NC}\n"
+aptly repo search runtel-trixie | head -25
+echo
+
+printf "${YELLOW}=== aptly repo show -with-packages runtel-trixie ===${NC}\n"
+aptly repo show -with-packages runtel-trixie | head -25
+echo
+
+printf "${RED}=== debs folder should be cleared after Ansible playbook ===${NC}\n"
+ls -alF /tmp/debs/
+ls -alF /tmp/debs/deb13/
+echo -e "${MAGENTA}================${NC}"
