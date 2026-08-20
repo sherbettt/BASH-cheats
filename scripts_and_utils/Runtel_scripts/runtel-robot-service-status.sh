6c973c84c32e0d67043ce24ab551aa8ebf140940
Kirill K <44464893+sherbettt@users.noreply.github.com>
2026-08-20 14:12:59 +0300

    Create runtel-robot-service-status.sh

---
+++ scripts_and_utils/Runtel_scripts/runtel-robot-service-status.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+# Определение цветов для оформления вывода
+GREEN='\033[0;32m'   # Зеленый - для команд
+BLUE='\033[0;34m'    # Синий - для заголовков
+NC='\033[0m'         # No Color - сброс цвета
+
+services=(
+        runtel-dialog-common-cpu
+        runtel-dialog-common-gpu-vulkan
+        runtel-dialog-llm-cpu
+        runtel-dialog-llm-gpu-vulkan
+        runtel-plugin-vits-tts
+        runtel-tinypbx
+)
+
+systemctl status "${services[@]/%/.service}" -l --no-pager || true
+echo ""  
+
+
+for p in "${services[@]}"; do
+    echo -e "${GREEN}▶ systemctl is-enabled $p${NC}"
+    systemctl is-enabled "$p" 2>/dev/null || echo "disabled/not found"
+done
+echo ""  
+
+for p in "${services[@]}"; do
+    echo -e "${GREEN}▶ apt policy $p${NC}"
+    apt-cache policy "$p" 2>/dev/null | head -3 || echo "  not found"
+    echo "" 
+done
+
+for p in "${services[@]}"; do
+    echo -e "${GREEN}▶ apt-cache policy $p${NC}"
+    apt-cache policy "$p" 2>/dev/null || echo "  not found"
+done
+
